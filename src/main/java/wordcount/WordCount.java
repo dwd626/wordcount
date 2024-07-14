117ae0c570f34457cd7af89b912a327a28b482d4
dw <dwd626@gmail.com>
2024-07-14 11:20:13 -0700

    Update exec steps

--- src/main/java/wordcount/WordCount.java
+++ src/main/java/wordcount/WordCount.java
@@ -6,9 +6,34 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Set;
 
+/**
+ * Write a program that reads a file and finds matches against a predefined set of words. There can be up to 10K entries in the list of predefined words. The output of the program should look something like this: 
+
+Predefined word           Match count 
+FirstName                           3500 
+LastName                           2700 
+Zipcode                                1601 
+
+Design:
+
+There are two steps:
+1. read the predefined words
+2. match the input file against the predefined words
+
+Considerations:
+- the size of the predefined words of 10K (each word max 256), and input file of 20M, in memory HashMap and StringBuilder suffice.
+- the predefined words may contain non-alpha characters such as "'-", but this is not known before reading the file.
+- run this as a standalone. for i/o, use simple System.in and System.out. All this may change in a different context.
+- to parse the input file, I compared String.split() and character scanning and chose the faster one.
+ 
+ */
 public class WordCount {
+	/** map to hold the original word (maintain original case) to the uppercase. */
+	private Map<String, String> originalWords;
+	
 	/** map to hold the predefined words and their counts */
 	private Map<String, Integer> predefinedWords;
 	
@@ -16,8 +41,35 @@ public class WordCount {
 	private Set<Character> predefinedChars;
 	
 	public WordCount() {
+		originalWords = new HashMap<>();
 		predefinedWords = new HashMap<>();
 		predefinedChars = new HashSet<>();
+	}
+	
+	public static void main(String[] args) {
+		String wordFile, textFile;
+		
+		if (args.length == 2) {
+			wordFile = args[0];
+			textFile = args[1];
+		} else {
+			Scanner reader = new Scanner(System.in);
+			System.out.println("Please provide two file names. Or enter the predefined word file name (full path):");
+			wordFile = reader.nextLine();
+			System.out.println("Enter the name of the file to be processed (full path):");
+			textFile = reader.nextLine();
+			reader.close();
+		}
+		
+		WordCount app = new WordCount();
+		Map<String, Integer> countedWords = app.countWords(textFile, wordFile);
+		
+		// output the results
+		StringBuilder buf = new StringBuilder("\nPredefined word\t\tMatch count\n");
+		for (Map.Entry<String, Integer> en: countedWords.entrySet()) {
+			buf.append(en.getKey()).append("\t").append(en.getValue()).append("\n");
+		}
+		System.out.println(buf.toString());
 	}
 	
 	/**
@@ -81,7 +133,11 @@ public class WordCount {
 				System.exit(1);
 			}
 		}
-		return predefinedWords;
+		Map<String, Integer> countWords = new HashMap<>();
+		for (Map.Entry<String, Integer> en: predefinedWords.entrySet()) {
+			countWords.put(originalWords.get(en.getKey()), en.getValue());
+		}
+		return countWords;
 	}
 	
 	/**
@@ -98,8 +154,12 @@ public class WordCount {
 		try {
 			r = new BufferedReader(new FileReader(listFile));
 			while ((line = r.readLine()) != null) {
-				// the line adds the word to the map of predefined words
-				predefinedWords.put(line.toUpperCase(), 0);
+				// keep the original words for output purpose
+				String lineUpper = line.toUpperCase();
+				originalWords.put(lineUpper, line);
+				
+				// adds the word to the map of predefined words
+				predefinedWords.put(lineUpper, 0);
 				
 				// In the predefined word file, each line is one word.
 				// This block collects special characters that may be part of a predefined word
